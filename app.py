549498473b9a100a97b93170a6a1326ee283f1bc
samm2209 <68995725+samm2209@users.noreply.github.com>
2021-05-30 09:19:19 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -36,31 +36,20 @@ def default():
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        entry_fname = request.form['fname']
-        new_fname = Contact(fname=entry_fname)
 
-        entry_lname = request.form['lname']
-        new_lname = Contact(lname=entry_lname)
-
-        entry_email = request.form['email']
-        new_email = Contact(email=entry_email)
-
-        entry_message = request.form['message']
-        new_message = Contact(message=entry_message)
+        contact = Contact(fname=request.form['fname'], lname=request.form['lname'],
+                          email=request.form['email'], message=request.form['message'])
 
         try:
-            db.session.add(new_fname)
-            db.session.add(new_lname)
-            db.session.add(new_email)
-            db.session.add(new_message)
+
+            db.session.add(contact)
             db.session.commit()
             return redirect('/')
         except:
             return "Error creating contact"
 
     else:
-        entries = Contact.query.order_by(Contact.date_added).all()
-        return render_template('contact.html', entries=entries)
+        return render_template('contact.html')
 
 
 if __name__ == "__main__":
